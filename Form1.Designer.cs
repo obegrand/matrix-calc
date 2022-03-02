1bfc53bec0e2b6bb5fb19b33f55e0d8b89e32b15
obegrand <obevan7777@yandex.ru>
2022-03-03 02:40:47 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace matrix_calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.matrixgrid1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.matrixgrid2 = new System.Windows.Forms.DataGridView();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.matrixresult = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.matrixgrid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrixgrid2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrixresult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // matrixgrid1
+            // 
+            this.matrixgrid1.AllowUserToAddRows = false;
+            this.matrixgrid1.AllowUserToDeleteRows = false;
+            this.matrixgrid1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.matrixgrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrixgrid1.Location = new System.Drawing.Point(29, 76);
+            this.matrixgrid1.Name = "matrixgrid1";
+            this.matrixgrid1.Size = new System.Drawing.Size(263, 223);
+            this.matrixgrid1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "random";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // matrixgrid2
+            // 
+            this.matrixgrid2.AllowUserToAddRows = false;
+            this.matrixgrid2.AllowUserToDeleteRows = false;
+            this.matrixgrid2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.matrixgrid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrixgrid2.Location = new System.Drawing.Point(334, 74);
+            this.matrixgrid2.Name = "matrixgrid2";
+            this.matrixgrid2.Size = new System.Drawing.Size(263, 223);
+            this.matrixgrid2.TabIndex = 4;
+            // 
+            // plus
+            // 
+            this.plus.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.plus.Location = new System.Drawing.Point(298, 98);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(30, 30);
+            this.plus.TabIndex = 5;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(298, 175);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(30, 30);
+            this.minus.TabIndex = 6;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Location = new System.Drawing.Point(298, 247);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(30, 30);
+            this.multiply.TabIndex = 7;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(38, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 26);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Матрицы";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(484, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 26);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "порядка";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(717, 814);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.load_Click);
+            // 
+            // matrixresult
+            // 
+            this.matrixresult.AllowUserToAddRows = false;
+            this.matrixresult.AllowUserToDeleteRows = false;
+            this.matrixresult.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.matrixresult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrixresult.Location = new System.Drawing.Point(130, 386);
+            this.matrixresult.Name = "matrixresult";
+            this.matrixresult.Size = new System.Drawing.Size(353, 223);
+            this.matrixresult.TabIndex = 19;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(510, 303);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 23);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "random";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(429, 303);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "Сохр. в .txt";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.trackBar1.LargeChange = 2;
+            this.trackBar1.Location = new System.Drawing.Point(149, 12);
+            this.trackBar1.Minimum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(329, 45);
+            this.trackBar1.TabIndex = 22;
+            this.trackBar1.Value = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(158, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(269, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(232, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(194, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.Control;
+            this.label8.Location = new System.Drawing.Point(307, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "6";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.Control;
+            this.label9.Location = new System.Drawing.Point(345, 44);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "7";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.SystemColors.Control;
+            this.label10.Location = new System.Drawing.Point(383, 44);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "8";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.SystemColors.Control;
+            this.label11.Location = new System.Drawing.Point(421, 44);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 31;
+            this.label11.Text = "9";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.SystemColors.Control;
+            this.label12.Location = new System.Drawing.Point(455, 44);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 13);
+            this.label12.TabIndex = 32;
+            this.label12.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(625, 721);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.matrixresult);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.matrixgrid2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.matrixgrid1);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Matrix Calculator (Melnikov A. D.)";
+            this.Load += new System.EventHandler(this.load_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.matrixgrid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrixgrid2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrixresult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView matrixgrid1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView matrixgrid2;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView matrixresult;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace matrix_calc
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int matrixwidth = 22+4;
+        int matrixrez = 35;
+        int matrixrank = 2;
+        int maxrand = 100;
+        int minrand = -99;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < matrixgrid1.ColumnCount; i++)
+            {
+                for (int j = 0; j < matrixgrid1.RowCount; j++)
+                {
+                    matrixgrid1[i, j].Value = random.Next(minrand, maxrand);
+                }
+            }
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+                for (int i = 0; i < matrixgrid1.RowCount; i++)
+                {
+                    for (int j = 0; j < matrixgrid1.ColumnCount; j++)
+                    {
+                        matrixresult.Columns[j].Width = matrixrez;
+                        matrixresult[i, j].Value = Convert.ToInt32(matrixgrid1[i, j].Value) + Convert.ToInt32(matrixgrid2[i, j].Value);
+                    }
+                }
+            
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < matrixgrid1.RowCount; i++)
+            {
+                for (int j = 0; j < matrixgrid1.ColumnCount; j++)
+                {
+                    matrixresult.Columns[j].Width = matrixrez;
+                    matrixresult[i, j].Value = Convert.ToInt32(matrixgrid1[i, j].Value) - Convert.ToInt32(matrixgrid2[i, j].Value);
+                }
+            }
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            int rows = matrixgrid1.RowCount;
+            int columns = matrixgrid2.ColumnCount;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    for (int inner = 0; inner < 2; inner++)
+                    {
+                        matrixresult[row, col].Value = Convert.ToInt32(matrixgrid1[row, inner].Value) * Convert.ToInt32(matrixgrid2[inner, col].Value);
+                    }
+                }
+            }
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            matrixresult.RowCount = matrixrank;
+            matrixresult.ColumnCount = matrixrank;
+            matrixgrid1.ColumnHeadersVisible = false;
+            matrixgrid1.RowHeadersVisible = false;
+            matrixgrid2.ColumnHeadersVisible = false;
+            matrixgrid2.RowHeadersVisible = false;
+            matrixresult.ColumnHeadersVisible = false;
+            matrixresult.RowHeadersVisible = false;
+            matrixgrid1.RowCount = matrixrank;
+            matrixgrid1.ColumnCount = matrixrank;
+            matrixgrid2.RowCount = matrixrank;
+            matrixgrid2.ColumnCount = matrixrank;
+            for (int i = 0; i < matrixgrid1.RowCount; i++)
+            {
+                for (int j = 0; j < matrixgrid1.ColumnCount; j++)
+                {
+                    matrixgrid1.Columns[j].Width = matrixwidth;
+                    matrixgrid2.Columns[j].Width = matrixwidth;
+                    matrixgrid1[i, j].Value = random.Next(100);
+                    matrixgrid2[i, j].Value = random.Next(100);
+                    matrixresult.Columns[j].Width = matrixrez;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < matrixgrid2.ColumnCount; i++)
+            {
+                for (int j = 0; j < matrixgrid2.RowCount; j++)
+                {
+                    matrixgrid2[i, j].Value = random.Next(minrand, maxrand);
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            matrixgrid1.RowCount = trackBar1.Value;
+            matrixgrid1.ColumnCount = trackBar1.Value;
+            matrixgrid2.RowCount = trackBar1.Value;
+            matrixgrid2.ColumnCount = trackBar1.Value;
+            matrixresult.RowCount = trackBar1.Value;
+            matrixresult.ColumnCount = trackBar1.Value;
+            for (int i = 0; i < matrixgrid1.RowCount; i++)
+            {
+                for (int j = 0; j < matrixgrid1.ColumnCount; j++)
+                {
+                    matrixgrid1.Columns[j].Width = matrixwidth;
+                    matrixgrid2.Columns[j].Width = matrixwidth; 
+                    matrixresult.Columns[j].Width = matrixrez;
+                }
+            }
+        }
+    }
+}
